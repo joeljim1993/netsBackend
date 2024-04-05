6c55cf2d05f137cc3d1b0c071d84167637fac8bd
joel jimenez <joel_jimenez@live.com>
2024-04-05 16:05:56 -0400

    data transfer Object

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { Mongoose } from 'mongoose';
+import { User, UserSchema } from './entities/user.entity';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  controllers: [AuthController],
+  providers: [AuthService],
+  imports : [
+    MongooseModule.forFeature([
+      {
+        name: User.name,
+        schema: UserSchema
+      }
+    ])
+  ]
+})
+
+export class AuthModule {}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,28 @@
+import { CreateUserDto } from './dto/create-user.dto';
+import { Injectable } from '@nestjs/common';
+import { UpdateAuthDto } from './dto/update-auth.dto';
+import { log } from 'console';
+
+@Injectable()
+export class AuthService {
+  create(createUserDto: CreateUserDto) {
+    console.log(createUserDto)
+    return 'This action adds a new auth';
+  }
+
+  findAll() { 
+    return `This action returns all auth`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} auth`;
+  }
+
+  update(id: number, updateAuthDto: UpdateAuthDto) {
+    return `This action updates a #${id} auth`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} auth`;
+  }
+}
